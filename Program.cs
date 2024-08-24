71161ccc91ba8de4af1f2f4a11eea2dc0f4c2186
TmSalviano <tiagomsalviano@outlook.com>
2024-08-24 18:12:40 -0300

    Page exception added for Entity Framework exceptions

--- Program.cs
+++ Program.cs
@@ -7,11 +7,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<BusinessDbContext>(opt => opt.UseSqlite(builder.Configuration.GetConnectionString("BusinessDbConnection")));
 builder.Services.AddDbContext<ServiceDbContext>(opt => opt.UseSqlite(builder.Configuration.GetConnectionString("ServiceDbConnection")));
 
+if (builder.Environment.IsDevelopment()) {
+    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+}
+
 var app = builder.Build();
 
 
 
 app.MapGet("/", () => "I am the Sun God!!! I am untatered and my legion knows no bounds!!!");
 
-
-app.Run();
+app.Run("http://localhost:3000");
